e5ceb68ab5c248126d9e80fe92b0066d91c7c14b
Najwafr14 <79633048+Najwafr14@users.noreply.github.com>
2024-04-09 22:10:16 +0700

    pertama

---
+++ script.js
@@ -0,0 +1,8 @@
+/*====untuk memberikan tampilan seperti mengetik dan memunculkan beberapa teks====*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Eid Mubarak!', 'Selamat Idul Fitri!', 'Mohon Maaf Lahir Batin!', 'Taqaballahu Minna wa Minkum!'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1500,
+    loop: true
+});
